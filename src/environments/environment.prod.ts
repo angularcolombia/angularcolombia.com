7f89dd0edf577bff3ac8d44d12882414ef4e9cb7
Mayra <mayrarodriguez@rovistack.com>
2019-01-22 14:14:28 -0500

    deploy config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCX6m7kZLQ4-iaRccOltsZuzZz8t0tk6qQ',
+    authDomain: 'community-template-3a3e7.firebaseapp.com',
+    databaseURL: 'https://community-template-3a3e7.firebaseio.com',
+    projectId: 'community-template-3a3e7',
+    storageBucket: 'community-template-3a3e7.appspot.com',
+    messagingSenderId: '841676598050'
+  }
 };
