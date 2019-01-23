42099f968c09b32665474d400e524881c167f013
UnJavaScripter <diegocoyt@gmail.com>
2019-01-23 21:04:11 +0000

    chore: update firebase settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCX6m7kZLQ4-iaRccOltsZuzZz8t0tk6qQ',
-    authDomain: 'community-template-3a3e7.firebaseapp.com',
-    databaseURL: 'https://community-template-3a3e7.firebaseio.com',
-    projectId: 'community-template-3a3e7',
-    storageBucket: 'community-template-3a3e7.appspot.com',
-    messagingSenderId: '841676598050'
+    apiKey: "AIzaSyCgHnacoojWqeSvsAHK9kNE7hbBb5lIK04",
+    authDomain: "angularcolombia.firebaseapp.com",
+    databaseURL: "https://angularcolombia.firebaseio.com",
+    projectId: "angularcolombia",
+    storageBucket: "angularcolombia.appspot.com",
+    messagingSenderId: "1085658434396"
   }
 };
